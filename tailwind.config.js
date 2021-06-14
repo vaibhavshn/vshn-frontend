3a0e845c49baa515494f7b9771b085052b5b0731
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2021-06-14 22:13:00 +0530

    chg: moved files to src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  purge: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
   darkMode: false,
   theme: {
     extend: {
